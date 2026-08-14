1ac7a0217fb6f8340bcb8209689d3d46ac6948ac
sohrab <sohrab@sohrabhossain.com>
2026-08-14 20:45:19 +0000

    add error handaling in user create script

--- code/scripts/create_user.py
+++ code/scripts/create_user.py
@@ -3,17 +3,21 @@ import sys
 from pathlib import Path
 from utils import create_user
 from core.config import settings
+from fastapi_users.exceptions import UserAlreadyExists
 
 parent_dir = Path(__file__).resolve().parent.parent
 
 sys.path.append(str(parent_dir))
 
 if __name__ == "__main__":
-    asyncio.run(
-        create_user(
-            email=settings.FIRST_SUPERUSER_EMAIL,
-            password=settings.FIRST_SUPERUSER_PASSWORD,
-            username=settings.FIRST_SUPERUSER_USERNAME,
-            display_name=settings.FIRST_SUPERUSER_DISPLAY_NAME,
+    try:
+        asyncio.run(
+            create_user(
+                email=settings.FIRST_SUPERUSER_EMAIL,
+                password=settings.FIRST_SUPERUSER_PASSWORD,
+                username=settings.FIRST_SUPERUSER_USERNAME,
+                display_name=settings.FIRST_SUPERUSER_DISPLAY_NAME,
             )
         )
+    except UserAlreadyExists:
+        print(f"User with email {settings.FIRST_SUPERUSER_EMAIL} already exists.")
